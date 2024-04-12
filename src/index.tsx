c867a7f74a1817abf0d48a40d9421c79bb346e09
Olha <olya.byalik26@gmail.com>
2024-04-12 16:36:29 +0300

    components

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,28 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-//without jsx
-// const el = React.createElement('h1', {id:'title'}, 'Hello title')
+const App = () => {
+    return (
+        <div>
+            <h1>Hello title</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officiis harum odio numquam at pariatur, similique asperiores
+                dolor error, aliquid nulla recusandae, ullam id magnam! Unde
+                repellat magni suscipit obcaecati molestias!
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officiis harum odio numquam at pariatur, similique asperiores
+                dolor error, aliquid nulla recusandae, ullam id magnam! Unde
+                repellat magni suscipit obcaecati molestias!
+            </p>
+        </div>
+    )
+}
 
-//with jsx
-const a = 10
-const el = <h1 id="title">Hello title, {a}</h1>
-root.render(<React.StrictMode>{el}</React.StrictMode>)
-
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
