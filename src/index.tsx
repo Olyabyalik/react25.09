db2c00ca666695dde395f29c2ac8c8549b9903e4
Olha <olya.byalik26@gmail.com>
2024-05-08 19:12:26 +0300

    deleted fiels

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Recusandae culpa aperiam eveniet dolorem dignissimos voluptas
-                corporis praesentium repudiandae! Doloribus commodi labore
-                alias, omnis repellat hic voluptatibus corporis? Earum, mollitia
-                enim.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Recusandae culpa aperiam eveniet dolorem dignissimos voluptas
-                corporis praesentium repudiandae! Doloribus commodi labore
-                alias, omnis repellat hic voluptatibus corporis? Earum, mollitia
-                enim.
-            </p>
-        </React.Fragment>
-    )
-}
-
 const App = () => {
-    return (
-        <>
-            <Header title="App.js" num={10} />
-            <Header title="React.js" />
-            <Content />
-        </>
-    )
+    return <></>
 }
 
 root.render(
